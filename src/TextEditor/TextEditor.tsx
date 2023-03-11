92d1f091006bca23cc762e4c3ee5e074736de435
buryattvoydrug <buryattvoydrug@gmail.com>
2023-03-11 18:19:49 +0300

    text editor view

--- src/TextEditor/TextEditor.tsx
+++ src/TextEditor/TextEditor.tsx
@@ -1,15 +1,23 @@
+import { Box } from '@material-ui/core';
 import { Editor } from 'draft-js'
 import { useEditorApi } from './context';
 
 export default function TextEditor() {
   const { note, onChange } = useEditorApi();
-
+  
   return (
     <div>
-      <Editor 
-        editorState={note.content}
-        onChange={onChange}
-      />
+      <Box
+        sx={{
+          borderBottom: "1px solid black",
+          fontSize: 18
+        }}
+        >
+        <Editor
+          editorState={note.content}
+          onChange={onChange}
+        />
+      </Box>
     </div>
   )
 }
